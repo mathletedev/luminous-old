ea918fe78219550c106ebfeb4a35d0beef2d1122
Neo Delta <mathletedev@gmail.com>
2022-04-30 21:39:32 -0700

    code formatting

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
 import { StatusBar } from "expo-status-bar";
+import { FC } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, world!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App: FC = () => (
+	<View style={styles.container}>
+		<Text>Hello, world!</Text>
+		<StatusBar style="auto" />
+	</View>
+);
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#ffffff",
+		alignItems: "center",
+		justifyContent: "center"
+	}
 });
+
+export default App;
